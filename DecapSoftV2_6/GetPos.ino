3afc7d0752db012b60517aede29b34beb1dcd5dc
Camille <camille.aussems@selexis.com>
2022-03-25 15:56:57 +0100

    Updated decapSoft to stable version on 2.5.2

--- DecapSoftV2_6/GetPos.ino
+++ DecapSoftV2_6/GetPos.ino
@@ -13,49 +13,45 @@ float movingAvg(float in[], int n, int x) {
 
 long GetPos(){
 
-  uint8_t vect[ly+1];
+  uint8_t vect[ly+1]; //length = 141
   vect[0] = floor(ly*0.5);
   for(int i = 1;i<=ly;i++){
-    vect[i] = vect[i-1] + pow(-1,i)*i; //
+    vect[i] = vect[i-1] + pow(-1,i)*i; //70, 70 + (-1)^i * i
   }
-
   float Result[10];
   uint8_t found = 0;
   uint8_t Pos[100];
   Result[0] = 0;
-  float avg[ly];
-  float mvt[ly-2*n];
-  uint8_t framebuff[320*240];
-  uint8_t *Pfbuff = framebuff;
+  float avg[ly]; //length = 140
+  float mvt[ly-2*n]; //length = 136
 
-  if (cam.grabFrame(fb,3000) == 0){
-    Pfbuff = fb.getBuffer();
+  if (cam.grab(Pfb) == 0){
     Serial.println("Snapshot");    
     }else{Serial.println("Cam capture failed");
       return 0;    
   }
-    // Cropavg ----------- Calcul le moving avg sur le frame buffer
+    // Cropavg - Compute the avg on each line in the cropped img
       uint8_t k = 0;
       for(uint8_t i = cropy[0]; i<=cropy[1]; i++){
         for(uint8_t j = cropx[0]; j<=cropx[1]; j++){
-              avg[k] += framebuff[i*320+j]; //Compute avg on the length () to obtain a 1xly vector
+              avg[k] += fb[i*320+j];
         }
-        k++; //k == i
+        
+        k++;
       }
       for(uint8_t i = 0; i<ly; i++){
-        avg[i] /= lx; //
+          avg[i] /= lx;
       }
-      //------------------
+      //-----Compute moving avg on the line avg computed before
     for(uint8_t i = n; i<ly-n; i++){
         mvt[i-n] = movingAvg(avg,n,i);
-      }    //Compute moving avg on the length averaged vector
-
+      }    
+    //-------- Compute the difference between
     for(uint8_t i = 2*n; i<= ly-2*n-1; i++){
-      bool diff1 = abs(mvt[vect[i]]-mvt[vect[i+2*n]]) > thres; //Change into i+2
-      bool diff2 = abs(mvt[vect[i]]-mvt[vect[i-2*n]]) > thres; // Change into i-2
-
+      bool diff1 = abs(mvt[vect[i]]-mvt[vect[i+2*n]]) > thres;
+      bool diff2 = abs(mvt[vect[i]]-mvt[vect[i-2*n]]) > thres;
       if(diff1 && diff2) {
-          Pos[found] = vect[i]; // Put in pos the x coord of the peak
+          Pos[found] = vect[i];
           found++;
         }
       }
@@ -64,37 +60,37 @@ long GetPos(){
     Serial.print("Capture found ");
     Serial.print(found);
     Serial.println(" markers.");
-    Pos[found] = 0; //Put 0 for the last Pos because we increment one time to much
+    Pos[found] = 0;
 
     uint8_t i,j = 0;
      k = 0;
 
     while(j < found -1 && k <10){
       i = 1;
-      Result[k] = Pos[j]; //Transfer Pos value into Result
-      while(abs(Pos[j]-Pos[j+1])<4 && j < found -1){ //Si Pos[j] et Pos[j+1] sont plus près que 4mm alors les adds
-        Result[k] += Pos[j+1];                       //pour faire la moyenne sur i
+      Result[k] = Pos[j];
+      while(abs(Pos[j]-Pos[j+1])<4 && j < found -1){
+        Result[k] += Pos[j+1];
         i++;j++;
       }
-      Result[k] /= i; //Result et un vecteurs avec les max 10 premiers pics, et ceux multiple sont moyenné
+      Result[k] /= i;
       k++;j++;
     }
 
 
     //Serial.println(Result[0]);
 
-    float pixtomm = 0.16286; //Should be right -- Depends on the distance to the cap might be wrong now
+    float pixtomm = 0.16286; //Should be right
     float pi = 3.14159265358979323846;  //is definitely right
-    //Le premier pic est le plus intéressant -> serait intéressant de voir la distance entre les différents pics
-    Result[0] = (Result[0] + cropy[0] - 120) * pixtomm; //
-    
+
+    Result[0] = (Result[0] + cropy[0] - 120) * pixtomm;
+
     return -ceil(stp1tour*(atan2(Result[0],24)/(2*pi)))+calibration;
 
 }
 
 //Calls GetPos() and check its reliability over 2 iterations
 long finalPos() {
-  cam.setStandby(false);
+  cam.standby(false);
 
   uint8_t tol = 100;
   uint8_t i = 0;
@@ -110,8 +106,8 @@ long finalPos() {
     temp2 = GetPos();
     delay(50);
     i++;
-  } //Fait une loop pour essayer d'avoir un résultat stable, tant que diff > tol
+  }
 
-  cam.setStandby(true);
+  cam.standby(true);
   return ceil((temp1+temp2)*0.5);
 }
